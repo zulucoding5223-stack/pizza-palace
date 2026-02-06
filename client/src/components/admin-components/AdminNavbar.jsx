bd6fde6a106576354821986b81bd16b34f0c8930
Bongokuhle <zulucoding5223@gmail.com>
2026-02-07 00:15:07 +0200

    Fixed admin navbar with full responsiveness

--- client/src/components/admin-components/AdminNavbar.jsx
+++ client/src/components/admin-components/AdminNavbar.jsx
@@ -30,54 +30,157 @@ const AdminNavbar = () => {
   return (
     <div>
       <section>
-        {!isOpen ? (
-          <div
-            className={`flex flex-col justify-between h-screen w-80 z-20 shadow-xl bg-blue-950 fixed p-5 shadow-black`}
-          >
-            <div>
-              <div className="flex items-center gap-3">
+        <div
+          className={`hidden flex-col justify-between h-screen w-80 z-20 shadow-xl bg-blue-950 fixed p-5 shadow-black md:flex`}
+        >
+          <div>
+            <div className="flex items-center gap-3">
+              <div className="w-full text-white flex items-center gap-4 text-xl font-bold shadow-md shadow-black rounded-full p-2">
+                <div className="w-fit p-1 rounded-full bg-white">
+                  <RiAdminFill color={"orange"} size={"2rem"} />
+                </div>
+                <p>Admin Dashboard</p>
+              </div>
+              <IoMenu
+                color="white"
+                size={"1.8rem"}
+                className="block md:hidden hover:cursor-pointer"
+                onClick={() => setIsOpen((prev) => !prev)}
+              />
+            </div>
+            <hr className="w-full text-gray-700 my-8" />
+            <ul className="flex items-center flex-col gap-3">
+              {adminLinks.map((adminLink, index) => {
+                const path =
+                  adminLink.name.toLocaleLowerCase() === "logout"
+                    ? "/"
+                    : `/admin/${adminLink.name.toLocaleLowerCase()}`;
+                return (
+                  <NavLink
+                    to={path}
+                    key={index}
+                    className={({ isActive }) =>
+                      `w-full text-white text-md flex items-center gap-3 p-2 hover:cursor-pointer  ${
+                        isActive
+                          ? "rounded-r-2xl bg-blue-700 hover:bg-blue-700"
+                          : "hover:bg-blue-900"
+                      }`
+                    }
+                  >
+                    <adminLink.icon color="white" size="1.5rem" />{" "}
+                    <p>{adminLink.name}</p>
+                  </NavLink>
+                );
+              })}
+            </ul>
+          </div>
+
+          <div>
+            <hr className="w-full text-gray-700 " />
+            <div className="">
+              <div className="w-full text-white flex items-center gap-4 text-xl font-bold shadow-md shadow-black rounded-full p-2 cursor-pointer">
+                <div className="w-fit rounded-full bg-white">
+                  <img
+                    src={profile}
+                    alt="profile-image"
+                    className="w-16 h-11 rounded-full object-center"
+                  />
+                </div>
+                <p className="w-full text-sm">Welcome, Admin {user.name}!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div
+          className={`flex flex-col justify-between h-screen ${isOpen ? "w-80" : "w-20"} z-20 shadow-xl bg-blue-950 fixed p-5 shadow-black md:hidden`}
+        >
+          <div>
+            <div className="flex items-start pl-1.5 gap-3 flex-col">
+              {isOpen ? (
                 <div className="w-full text-white flex items-center gap-4 text-xl font-bold shadow-md shadow-black rounded-full p-2">
                   <div className="w-fit p-1 rounded-full bg-white">
                     <RiAdminFill color={"orange"} size={"2rem"} />
                   </div>
                   <p>Admin Dashboard</p>
                 </div>
-                <IoMenu
-                  color="white"
-                  size={"1.8rem"}
-                  className="block md:hidden hover:cursor-pointer"
-                  onClick={() => setIsOpen((prev) => !prev)}
-                />
-              </div>
-              <hr className="w-full text-gray-700 my-8" />
-              <ul className="flex items-center flex-col gap-3">
-                {adminLinks.map((adminLink, index) => {
-                  const path =
-                    adminLink.name.toLocaleLowerCase() === "logout"
-                      ? "/"
-                      : `/admin/${adminLink.name.toLocaleLowerCase()}`;
-                  return (
-                    <NavLink
-                      to={path}
-                      key={index}
-                      className={({ isActive }) =>
-                        `w-full text-white text-md flex items-center gap-3 p-2 hover:cursor-pointer  ${
-                          isActive
-                            ? "rounded-r-2xl bg-blue-700 hover:bg-blue-700"
-                            : "hover:bg-blue-900"
-                        }`
-                      }
-                    >
-                      <adminLink.icon color="white" size="1.5rem" />{" "}
-                      <p>{adminLink.name}</p>
-                    </NavLink>
-                  );
-                })}
-              </ul>
-            </div>
+              ) : (
+                <div className="w-fit p-1 rounded-full bg-white -ml-1">
+                  <RiAdminFill color={"orange"} size={"2rem"} />
+                </div>
+              )}
 
-            <div>
-              <hr className="w-full text-gray-700 " />
+              <IoMenu
+                color="white"
+                size={"1.8rem"}
+                className="block md:hidden hover:cursor-pointer"
+                onClick={() => setIsOpen((prev) => !prev)}
+              />
+            </div>
+            <hr className="w-full text-gray-700 my-8" />
+            {isOpen ? (
+              <>
+                <ul className="flex items-center flex-col gap-3">
+                  {adminLinks.map((adminLink, index) => {
+                    const path =
+                      adminLink.name.toLocaleLowerCase() === "logout"
+                        ? "/"
+                        : `/admin/${adminLink.name.toLocaleLowerCase()}`;
+                    return (
+                      <NavLink
+                        to={path}
+                        key={index}
+                        onClick={() => {
+                          setIsOpen(false);
+                        }}
+                        className={({ isActive }) =>
+                          `w-full text-white text-md flex items-center gap-3 p-2 hover:cursor-pointer  ${
+                            isActive
+                              ? "rounded-r-2xl bg-blue-700 hover:bg-blue-700"
+                              : "hover:bg-blue-900"
+                          }`
+                        }
+                      >
+                        <adminLink.icon color="white" size="1.5rem" />{" "}
+                        <p>{adminLink.name}</p>
+                      </NavLink>
+                    );
+                  })}
+                </ul>
+              </>
+            ) : (
+              <>
+                <ul className="flex items-center flex-col gap-3">
+                  {adminLinks.map((adminLink, index) => {
+                    const path =
+                      adminLink.name.toLocaleLowerCase() === "logout"
+                        ? "/"
+                        : `/admin/${adminLink.name.toLocaleLowerCase()}`;
+
+                    return (
+                      <NavLink
+                        to={path}
+                        key={index}
+                        className={({ isActive }) =>
+                          `w-full text-white text-md flex items-center gap-3 p-2 hover:cursor-pointer  ${
+                            isActive
+                              ? "rounded-r-2xl bg-blue-700 hover:bg-blue-700"
+                              : "hover:bg-blue-900"
+                          }`
+                        }
+                      >
+                        <adminLink.icon color="white" size="1.5rem" />{" "}
+                      </NavLink>
+                    );
+                  })}
+                </ul>
+              </>
+            )}
+          </div>
+
+          <div>
+            <hr className="w-full text-gray-700 mb-5" />
+            {isOpen ? (
               <div className="">
                 <div className="w-full text-white flex items-center gap-4 text-xl font-bold shadow-md shadow-black rounded-full p-2 cursor-pointer">
                   <div className="w-fit rounded-full bg-white">
@@ -90,53 +193,7 @@ const AdminNavbar = () => {
                   <p className="w-full text-sm">Welcome, Admin {user.name}!</p>
                 </div>
               </div>
-            </div>
-          </div>
-        ) : (
-          <div className="flex flex-col justify-between h-screen w-20 z-20 shadow-xl bg-blue-950 fixed p-5 shadow-black md:hidden">
-            <div>
-              <div className="flex items-center gap-3 flex-col">
-                <div className="w-fit p-1 rounded-full bg-white">
-                  <RiAdminFill color={"orange"} size={"2rem"} />
-                </div>
-
-                <IoMenu
-                  color="white"
-                  size={"1.8rem"}
-                  className="block md:hidden hover:cursor-pointer"
-                  onClick={() => setIsOpen((prev) => !prev)}
-                />
-              </div>
-              <hr className="w-full text-gray-700 my-8" />
-              <ul className="flex items-center flex-col gap-3">
-                {adminLinks.map((adminLink, index) => {
-                  const path =
-                    adminLink.name.toLocaleLowerCase() === "logout"
-                      ? "/"
-                      : `/admin/${adminLink.name.toLocaleLowerCase()}`;
-
-                  return (
-                    <NavLink
-                      to={path}
-                      key={index}
-                      onClick={() => {setIsOpen(false)}}
-                      className={({ isActive }) =>
-                        `w-full text-white text-md flex items-center gap-3 p-2 hover:cursor-pointer  ${
-                          isActive
-                            ? "rounded-r-2xl bg-blue-700 hover:bg-blue-700"
-                            : "hover:bg-blue-900"
-                        }`
-                      }
-                    >
-                      <adminLink.icon color="white" size="1.5rem" />{" "}
-                    </NavLink>
-                  );
-                })}
-              </ul>
-            </div>
-
-            <div>
-              <hr className="w-full text-gray-700 mb-5" />
+            ) : (
               <div className="">
                 <div className="w-fit rounded-full bg-white">
                   <img
@@ -146,9 +203,9 @@ const AdminNavbar = () => {
                   />
                 </div>
               </div>
-            </div>
+            )}
           </div>
-        )}
+        </div>
       </section>
     </div>
   );
